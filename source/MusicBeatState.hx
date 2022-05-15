b1608cd07ac9d6e7dfe1b0bb64ca758d68de2695
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-15 11:06:06 -0300

    Add files via upload

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -1,14 +1,21 @@
 package;
 
-import flixel.FlxBasic;
 #if windows
 import Discord.DiscordClient;
 #end
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import openfl.Lib;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
+import flixel.addons.transition.FlxTransitionableState;
 import flixel.addons.ui.FlxUIState;
+import flixel.math.FlxRect;
+import flixel.util.FlxTimer;
+#if mobileC
+import ui.FlxVirtualPad;
+#end
+import flixel.input.actions.FlxActionInput;
 
 class MusicBeatState extends FlxUIState
 {
@@ -17,35 +24,42 @@ class MusicBeatState extends FlxUIState
 
 	private var curStep:Int = 0;
 	private var curBeat:Int = 0;
-	private var curDecimalBeat:Float = 0;
 	private var controls(get, never):Controls;
 
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
+	
 
-	private var assets:Array<FlxBasic> = [];
+	#if mobileC
+	var _virtualpad:FlxVirtualPad;
 
-	override function add(Object:flixel.FlxBasic):flixel.FlxBasic
-	{
-		if (FlxG.save.data.optimize)
-			assets.push(Object);
-		return super.add(Object);
+	var trackedinputs:Array<FlxActionInput> = [];
+
+	// adding virtualpad to state
+	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action);
+		_virtualpad.alpha = 0.75;
+		add(_virtualpad);
+		controls.setVirtualPad(_virtualpad, DPad, Action);
+		trackedinputs = controls.trackedinputs;
+		controls.trackedinputs = [];
+
+		#if android
+		controls.addAndroidBack();
+		#end
 	}
 
-	public function clean()
-	{
-		if (FlxG.save.data.optimize)
-		{
-			for(i in assets)
-			{
-				remove(i);
-			}
-		}
+	override function destroy() {
+		controls.removeFlxInput(trackedinputs);
+
+		super.destroy();
 	}
+	#else
+	public function addVirtualPad(?DPad, ?Action){};
+	#end
 
 	override function create()
 	{
-		TimingStruct.clearTimings();
 		(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
 		if (transIn != null)
@@ -70,94 +84,13 @@ class MusicBeatState extends FlxUIState
 	override function update(elapsed:Float)
 	{
 		//everyStep();
-		/*var nextStep:Int = updateCurStep();
+		var oldStep:Int = curStep;
 
-		if (nextStep >= 0)
-		{
-			if (nextStep > curStep)
-			{
-				for (i in curStep...nextStep)
-				{
-					curStep++;
-					updateBeat();
-					stepHit();
-				}
-			}
-			else if (nextStep < curStep)
-			{
-				//Song reset?
-				curStep = nextStep;
-				updateBeat();
-				stepHit();
-			}
-		}*/
+		updateCurStep();
+		updateBeat();
 
-		if (Conductor.songPosition < 0)
-			curDecimalBeat = 0;
-		else
-		{
-			if (TimingStruct.AllTimings.length > 1)
-			{
-				var data = TimingStruct.getTimingAtTimestamp(Conductor.songPosition);
-
-				FlxG.watch.addQuick("Current Conductor Timing Seg", data.bpm);
-
-				Conductor.crochet = ((60 / data.bpm) * 1000);
-
-				var step = ((60 / data.bpm) * 1000) / 4;
-				var startInMS = (data.startTime * 1000);
-
-				curDecimalBeat = data.startBeat + (((Conductor.songPosition/1000) - data.startTime) * (data.bpm / 60));
-				var ste:Int = Math.floor(data.startStep + ((Conductor.songPosition - startInMS) / step));
-				if (ste >= 0)
-				{
-					if (ste > curStep)
-					{
-						for (i in curStep...ste)
-						{
-							curStep++;
-							updateBeat();
-							stepHit();
-						}
-					}
-					else if (ste < curStep)
-					{
-						trace("reset steps for some reason?? at " + Conductor.songPosition);
-						//Song reset?
-						curStep = ste;
-						updateBeat();
-						stepHit();
-					}
-				}
-			}
-			else
-			{
-				curDecimalBeat = (Conductor.songPosition / 1000) * (Conductor.bpm/60);
-				var nextStep:Int = Math.floor(Conductor.songPosition / Conductor.stepCrochet);
-				if (nextStep >= 0)
-				{
-					if (nextStep > curStep)
-					{
-						for (i in curStep...nextStep)
-						{
-							curStep++;
-							updateBeat();
-							stepHit();
-						}
-					}
-					else if (nextStep < curStep)
-					{
-						//Song reset?
-						trace("(no bpm change) reset steps for some reason?? at " + Conductor.songPosition);
-						curStep = nextStep;
-						updateBeat();
-						stepHit();
-					}
-				}
-				Conductor.crochet = ((60 / Conductor.bpm) * 1000);
-			}
-		}
-
+		if (oldStep != curStep && curStep > 0)
+			stepHit();
 
 		if (FlxG.save.data.fpsRain && skippedFrames >= 6)
 			{
@@ -178,13 +111,13 @@ class MusicBeatState extends FlxUIState
 
 	private function updateBeat():Void
 	{
-		lastBeat = curBeat;
+		lastBeat = curStep;
 		curBeat = Math.floor(curStep / 4);
 	}
 
 	public static var currentColor = 0;
 
-	private function updateCurStep():Int
+	private function updateCurStep():Void
 	{
 		var lastChange:BPMChangeEvent = {
 			stepTime: 0,
@@ -197,11 +130,12 @@ class MusicBeatState extends FlxUIState
 				lastChange = Conductor.bpmChangeMap[i];
 		}
 
-		return lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
+		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
 	}
 
 	public function stepHit():Void
 	{
+
 		if (curStep % 4 == 0)
 			beatHit();
 	}
